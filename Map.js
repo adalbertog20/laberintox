727e2b1bc0731817d22d39f4b73da916332bf7c8
adalbertog20 <adalbertog_20@alu.uabcs.mx>
2023-09-12 11:52:20 -0700

    creaciond el mapa

--- Map.js
+++ Map.js
@@ -3,8 +3,41 @@ import Block from "./Block.js";
 export default class Map {
     constructor() {
         this.w = [
-          this.w1 = new Block(20, 40),
-          this.w2 = new Block(40, 40)
+          new Block(20, 0),
+          new Block(0, 40),
+          new Block(20, 40),
+          new Block(40, 40),
+          new Block(60, 40),
+          new Block(60, 20),
+          new Block(60, 20),
+          new Block(80, 20),
+          new Block(100, 20),
+          new Block(100, 40),
+          new Block(100, 60),
+          new Block(100, 80),
+          new Block(120, 80),
+
+          new Block(140, 100),
+          new Block(160, 100),
+          new Block(180, 100),
+          new Block(200, 100),
+          new Block(220, 100),
+          new Block(240, 100),
+          new Block(260, 100),
+
+          new Block(140, 0),
+          new Block(140, 20),
+          new Block(140, 40),
+          new Block(160, 40),
+
+          new Block(160, 60),
+          new Block(180, 60),
+          new Block(200, 60),
+          new Block(220, 60),
+          new Block(240, 60),
+          new Block(260, 60),
+          new Block(280, 60),
+
         ];
       }
       paint(ctx) {
